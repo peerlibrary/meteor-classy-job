be35b79b1ad86e5cf414446a420f09d820ab8569
Mitar <mitar.git@tnode.com>
2016-08-02 16:47:09 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'peerlibrary:classy-job',
   summary: "Class-based wrapper around job collection",
-  version: '0.3.0',
+  version: '0.4.0',
   git: 'https://github.com/peerlibrary/meteor-classy-job.git'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.1.0.3');
+  api.versionsFrom('1.4.0.1');
 
   // Core dependencies.
   api.use([
@@ -19,7 +19,7 @@ Package.onUse(function (api) {
 
   // 3rd party dependencies.
   api.use([
-    'vsivsi:job-collection@1.2.3',
+    'vsivsi:job-collection@1.4.0',
     'peerlibrary:stacktrace@0.2.0'
   ]);
 
